bee01901ae32ac29ab7c33c5e3a89abcd0c42a58
bmuhammad <briancmuhammad@yahoo.com>
2025-03-13 12:33:13 -0700

    added content secure policy in layout page to mitigate cross site scriptiong. blocks inline javascript. moved js code to its own file.

--- WebApiAuthentication.ClientWithAPI/wwwroot/js/site.js
+++ WebApiAuthentication.ClientWithAPI/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+if (document.getElementById('callApiButton')) {
+
+    document.getElementById('callApiButton').addEventListener('click', function () {
+        fetch('/api/claims')
+            .then(response => response.json())
+            .then(data => {
+                let resultDiv = document.getElementById('apiResult');
+                resultDiv.innerHTML = '<h3>API Call Results</h3><ul>';
+                data.forEach(claim => {
+                    debugger;
+                    resultDiv.innerHTML += `<li>${claim.type}: ${claim.value}</li>`;
+                });
+                resultDiv.innerHTML += '</ul>';
+            })
+            .catch(error => console.error('Error:', error));
+    });
+}
